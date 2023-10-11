4a711e1ae4d8ff4700f1239fc4eb599d5d7f3b10
Ivan Birkmaier <ivan.birkmaier@INFO.TIME-FOR-KIDS>
2023-10-11 21:00:27 +0200

    Initial try for implementing the first unit test for the utils script.

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,11 +1,18 @@
 import dotenv as de
+# This will load the environment variables from .env file
+de.load_dotenv()
 import os
 
 
-de.load_dotenv()  # This will load the environment variables from .env file
+'''
+This methode returns the enviroment variables which have to be set in an .env file at the rootlevel of this respository.
+For this script you have to install dotenv-package in your python-enviroment. For getting the right result you have to name your
+enviroment variables in the your .env file as written in for example in os.getenv('CLOCKWORK_API_TOKEN').
 
-
-# Gibt die Enviroment Variablen für ein bestimmtes Produkt zurück. Die Enviroment Variablen müssen im .env file festgelgt werden
+NOTE:
+Aways make sure that the .env file is set in your .gitignore file so that your personal information will never be pushed to any
+remote brunch. This is to make sure that there is no possibilty to spread your personal information trough the internet.
+'''
 def enviroment_variables(produkt: str):
     if produkt.lower() == "clockwork":
         CLOCKWORK_API_TOKEN = os.getenv('CLOCKWORK_API_TOKEN')
@@ -17,34 +24,26 @@ def enviroment_variables(produkt: str):
         JIRA_API_TOKEN = os.getenv('JIRA_API_TOKEN')
         WORKSPACE_ID = os.getenv('WORKSPACE_ID')
         return COMPANY_SUBDOMAIN, USER_MAIL, JIRA_API_TOKEN, WORKSPACE_ID
+    # For the test in file dict = src.test.utils.utils.
+    if produkt.lower() == "test":
+        TEST_API_TOKEN = "12345Token"
+        TEST_USER_ID = "max.mustermann@test.com"
+        return TEST_API_TOKEN, TEST_USER_ID
 
-# Diese Method nimmt ein String (Wort) entgegen und gibt ein Kürzel-Zurück
-
+'''
+Creates an abbreviation from a giving string by comparing the created abbreviation to a list of already existing abbreviation. If the abbreviation already exist in the given list
+then the funktion modifies the created abbreviation until it created an uique abbreviation for the given string.
+Example: 
+  string = "TestString" (initial expected abbreviation = "TE"
+  existing_abbreviations = ["AB","TE"]
+Because the initial abbreviation is already existing in the list of given abbreviation exist, the methode returns "TES". 
+Otherwise the methode whould return "TE".
+'''
 def create_abbreviation(string: str, existing_abbreviations: list):
-    splits = string.split('_')  # Split the word at underscores
+    # Split the word at spaces if existing
+    splits = string.split()
+    # Creates a
     abbreviation = ''.join([split[0].upper() for split in splits if split])
-
-    # Add characters to the abbreviation until it's unique or all characters are used
-    i = 1
-    all_chars = ''.join(splits).upper()
-    while abbreviation in existing_abbreviations and i < len(all_chars):
-        abbreviation += all_chars[i]
-        i += 1
-
-    # If we've used up all characters and the abbreviation is still not unique, add numbers
-    number = 1
-    while abbreviation in existing_abbreviations:
-        abbreviation = abbreviation[:-1] if number > 1 else abbreviation  # remove the previous number
-        abbreviation += str(number)
-        number += 1
-
-    return abbreviation
-
-
-def create_abbreviation(string: str, existing_abbreviations: list):
-    splits = string.split()  # Split the word at spaces
-    abbreviation = ''.join([split[0].upper() for split in splits if split])
-
     # If the abbreviation is only one character, use the second character of the first word part
     if len(abbreviation) == 1:
         abbreviation += splits[0][1].upper()
@@ -64,3 +63,26 @@ def create_abbreviation(string: str, existing_abbreviations: list):
         char_index += 1
 
     return abbreviation
+
+
+# Diese Method nimmt ein String (Wort) entgegen und gibt ein Kürzel-Zurück
+
+# def create_abbreviation(string: str, existing_abbreviations: list):
+#     splits = string.split('_')  # Split the word at underscores
+#     abbreviation = ''.join([split[0].upper() for split in splits if split])
+#
+#     # Add characters to the abbreviation until it's unique or all characters are used
+#     i = 1
+#     all_chars = ''.join(splits).upper()
+#     while abbreviation in existing_abbreviations and i < len(all_chars):
+#         abbreviation += all_chars[i]
+#         i += 1
+#
+#     # If we've used up all characters and the abbreviation is still not unique, add numbers
+#     number = 1
+#     while abbreviation in existing_abbreviations:
+#         abbreviation = abbreviation[:-1] if number > 1 else abbreviation  # remove the previous number
+#         abbreviation += str(number)
+#         number += 1
+#
+#     return abbreviation
--- test/utils/test_utils.py
+++ test/utils/test_utils.py
@@ -1,2 +1,37 @@
-def test_something():
-    assert True == False  # add assertion here
+from src.utils import utils as utl
+
+# Script_variables:
+TEST_RUNS_test_create_abbreviation = [["Second_Test", "ST", ["AB", "TI"]]]
+
+
+# utl.enviroment_variables("Produktname als string")
+# utl.create_abbreviation("Name für Kürzel", listeExistierenderKürzel = [])
+
+def test_create_abbreviation(string = "Initial Test", abbreviation = "IT", existing_abbreviations = ["AB", "TI"]):
+    # Check if abbreviation already exist in list existing_abbreviations. If not included contiue with the test.
+    # If it already exist, then test fails.
+    if abbreviation not in existing_abbreviations:
+        # Get the abbreviation from the methode.
+        abbreviation_from_methode = utl.create_abbreviation(string,existing_abbreviations)
+        # Check if the expected abbrevation is the same as the returned abbrevation from the methode.
+        # If true (they are the same then continue with the test. If false then fail the test.
+        if abbreviation == abbreviation_from_methode:
+            # Check if there is still an run left in the Script_variable.
+            if TEST_RUNS_test_create_abbreviation:
+                # If run left, then take this run and delete ist from the list of runs.
+                # Does this until there is no run left.
+                run = TEST_RUNS_test_create_abbreviation.pop(0)
+                # Executes the same methode with the new run from all Testruns.
+                test_create_abbreviation(run[0],run[1],run[2])
+            # If this methode executes until here, then the test is done.
+            assert True == True
+        else:
+            assert False == True
+    else:
+        assert False == True
+
+
+if __name__ == "__main__":
+    test_create_abbreviation()
+    print("Test")
+
