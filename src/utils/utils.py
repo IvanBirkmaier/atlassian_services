c10af970ab61a432917aad96f2212f08e72d7ede
Ivan Birkmaier <ivan.birkmaier@INFO.TIME-FOR-KIDS>
2023-10-12 16:55:38 +0200

    finished dokumentation for utils and test for utils. added .env-template.txt

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -11,10 +11,16 @@ de.load_dotenv()
 
 
 '''
+DESCRIPTION:
 This methode returns the enviroment variables which have to be set in an .env file at the rootlevel of this respository.
 For this script you have to install dotenv-package in your python-enviroment. For getting the right result you have to name your
 enviroment variables in the your .env file as written in for example in os.getenv('CLOCKWORK_API_TOKEN').
-
+-------------------------------------------------------------------------------------------
+INPUT:
+produkt: String with the atlassian Produkt for which you need the env-variables
+-------------------------------------------------------------------------------------------
+RETURN: Every env-variable as string
+-------------------------------------------------------------------------------------------
 NOTE:
 Aways make sure that the .env file is set in your .gitignore file so that your personal information will never be pushed to any
 remote brunch. This is to make sure that there is no possibilty to spread your personal information trough the internet.
@@ -38,6 +44,7 @@ def enviroment_variables(produkt: str):
 
 
 '''
+DESCRIPTION:
 Creates an abbreviation from a giving string by comparing the created abbreviation to a list of already existing abbreviation. If the abbreviation already exist in the given list
 then the funktion modifies the created abbreviation until it created an uique abbreviation for the given string.
 Example: 
@@ -45,6 +52,13 @@ Example:
   existing_abbreviations = ["AB","TE"]
 Because the initial abbreviation is already existing in the list of given abbreviation exist, the methode returns "TES". 
 Otherwise the methode whould return "TE".
+-------------------------------------------------------------------------------------------
+INPUT:
+string = String for which you would like to get the abbreviations
+existing_abbreviations = List of strings with existing abbreviations (also empty list is possible)
+-------------------------------------------------------------------------------------------
+RETURN:
+abbreviation = Stirng with abbreviation
 '''
 def create_abbreviation(string: str, existing_abbreviations: list):
     string = re.sub("[^a-zA-ZäöüÄÖÜß ]", "", string)
@@ -84,14 +98,20 @@ def create_abbreviation(string: str, existing_abbreviations: list):
 
 
 '''
+DESCRIPTION:
 This function can extract information from HTML-page/content. It takes the content (f.e. HTML-page, or any other subset of HTML-tags) and extract the given
 content for the given extractor: For example content is an HTML page with two tables on it <table>. If we set extractor = "table" then the returned result 
-will be a list of the two tables from the page. This function needs also an parser. The libeary which is used for extracting the content is Beautiful Soup.
+will be a ResultSet with the entries of the type SoupStrainer (both from BeautifulSoup) of the two tables from the page. This function needs also an parser. The libeary which is used for extracting the content is Beautiful Soup.
 Beautiful soup provieds a set of parser. 
+-------------------------------------------------------------------------------------------
+INPUT:
 content = HTML-Page, or subset
 extractor = HTML-Element which you want to be extracted
-parser = Can be one of: 'html.parser, 'lxml', 'lxml-xml', 'html5lib'
-
+parser = Can be one of: 'html.parser', 'lxml', 'lxml-xml', 'html5lib'
+-------------------------------------------------------------------------------------------
+RETURN:
+result = will be a ResultSet with the entries of the type SoupStrainer (both from BeautifulSoup)
+-------------------------------------------------------------------------------------------
 NOTE: With the methode .get_text from BeautifulSoup you can acess the text value of the extracted HTML elements 
 '''
 def information_extractor(content:str, extractor: str, parser: str):
@@ -99,9 +119,3 @@ def information_extractor(content:str, extractor: str, parser: str):
     result = soup.find_all(extractor)
     return result
 
-# def information_extractor(content, extractor: str, parser: str):
-#     if not isinstance(content, str):
-#         raise TypeError(f"Die übergebene Variable item muss für die Methode informationExtractor() ein String sein. Möglich mit str(item) beim übergeben in die Methode")
-#     soup = BeautifulSoup(content, parser)
-#     result = soup.find_all(extractor)
-#     return result
---
+++ test/utils/unit_test_utils.py
@@ -0,0 +1,89 @@
+from bs4 import BeautifulSoup, ResultSet, Tag
+from src.utils import utils as utl
+
+# Script_variables:
+TEST_RUNS_test_create_abbreviation = [["Second_Test", "SE", ["AB", "TI"]], ["thridTest", "THR", ["AB", "TH"]],
+                                      ["A", "AA", ["AB", "TI"]], ["fifth test", "FTF", ["AB", "FT"]],
+                                      ["$4 444", "AA", ["AB", "FT"]], ["$4 444", "AC", ["AB", "FT", "AA"]],
+                                      ["$4 444", "AD", ["AB", "FT", "AA", "AC"]]]
+
+TEST_RUNS_test_information_extractor = [
+    ["<html><th>Second Test</th></html>", "th", [["th", "Second Test"]], "html.parser"],
+    ["<h1><p>Third Test a</p><p>Third Test b</p><p>Third Test c</p></h1>", "p",
+     [["p", "Third Test a"], ["p", "Third Test b"], ["p", "Third Test c"]], "html.parser"],
+    ["<table><tbody><tr>Unit Test</tr></tbody></table>", "tbody", [["tbody", "<tr>Unit Test</tr>"]], "html.parser"]]
+
+#
+# TEST_RUNS_test_information_extractor = [
+#     ["<table><tbody><tr>Unit Test</tr></tbody></table>", "tbody", [["tbody", "<tr>Unit Test</tr>"]], "html.parser"]]
+
+'''
+DESCRIPTION:
+This is an recrusiv test-methode where multiple test runs, which are defined in the Script_variables. It tests the methode of. 
+Teste methode: create_abbreviation
+-------------------------------------------------------------------------------------------
+FUNCTION WHICH IS TESTED:
+create_abbreviation(string, existing_abbreviations)
+'''
+def test_create_abbreviation(string="Initial Test", abbreviation="IT", existing_abbreviations=["AB", "TI"]):
+    # Get the abbreviation from the method.
+    abbreviation_from_method = utl.create_abbreviation(string, existing_abbreviations)
+    # Check if the expected abbreviation matches the abbreviation returned by the method.
+    if abbreviation == abbreviation_from_method:
+        # Check if there's still a run left in the Script_variable.
+        if TEST_RUNS_test_create_abbreviation:
+            # If a run remains, take that run and remove it from the list of runs.
+            run = TEST_RUNS_test_create_abbreviation.pop(0)
+            # Execute the same method with the new run from all the test runs.
+            test_create_abbreviation(run[0], run[1], run[2])
+        # If this method executes up to here, the test is complete.
+        assert abbreviation == abbreviation_from_method
+    else:
+        raise AssertionError(f"Expected abbreviation {abbreviation}, but received {abbreviation_from_method}.")
+
+
+'''
+This test function tests the returns of the function enviroment_variables for the "test" input. 
+-------------------------------------------------------------------------------------------
+FUNCTION WHICH IS TESTED:
+enviroment_variables(product)
+'''
+def test_enviroment_variables():
+    EXPECTED_OUTPUT_TEST_API_TOKEN = "12345Token"
+    EXPECTED_OUTPUT_TEST_USER_ID = "max.mustermann@test.com"
+    TEST_API_TOKEN, TEST_USER_ID = utl.enviroment_variables("Test")
+    assert EXPECTED_OUTPUT_TEST_API_TOKEN == TEST_API_TOKEN and EXPECTED_OUTPUT_TEST_USER_ID == TEST_USER_ID
+
+
+'''
+DESCRIPTION:
+This is an recrusiv test-methode where multiple test runs, which are defined in the Script_variables.
+-------------------------------------------------------------------------------------------
+FUNCTION WHICH IS TESTED:
+information_extractor(content,extractor,parser)
+'''
+def test_information_extractor(content="<table><th>Unit Test</th></table>", extractor="th",
+                               expected_result_string=[["th", "Unit Test"]], parser="html.parser"):
+    soup = BeautifulSoup('', 'html.parser')
+    result_from_methode = utl.information_extractor(content, extractor, parser)
+    # New tag obeject from BeautifulSoup for testing
+    expected_result_set = []
+    for test in expected_result_string:
+        new_tag = soup.new_tag(test[0])
+        inner_content = BeautifulSoup(test[1], parser)
+        for content in inner_content.contents:
+            new_tag.append(content)
+        # adds a new created tag element to the expected_result_set list
+        expected_result_set.append(new_tag)
+    expected_result_set = ResultSet(None, expected_result_set)
+    if expected_result_set == result_from_methode:
+        if TEST_RUNS_test_information_extractor:
+            # If a run remains, take that run and remove it from the list of runs.
+            run = TEST_RUNS_test_information_extractor.pop(0)
+            # Execute the same method with the new run from all the test runs.
+            test_information_extractor(run[0], run[1], run[2])
+        # If this method executes up to here, the test is complete.
+        assert expected_result_set == result_from_methode
+    else:
+        raise AssertionError(
+            f"Expected result set {expected_result_set}, but received result set: {result_from_methode}.")
